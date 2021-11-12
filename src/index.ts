89c8d955cfb1c65ca5dbd7fdf65d7dbb709ac6c9
samroodAli <devsamrood@gmail.com>
2021-11-12 18:06:38 +0530

    add errors and middlewares files from auth service and export them

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
-interface Color {
-  red: number,
-  blue: number,
-  green:number
-}
+export * from "./errors/bad-request-error"
+export * from "./errors/custom-error"
+export * from "./errors/database-connection-error"
+export * from "./errors/not-authorised-error"
+export * from "./errors/not-found"
+export * from "./errors/request-validation-error"
 
-const color: Color = {
-  red: 10,
-  blue: 10,
-  green:10
-}
-
-console.log(color)
+export * from "./middlewares/current-user"
+export * from "./middlewares/error-handler"
+export * from "./middlewares/require-auth"
+export * from "./middlewares/validate-request"
