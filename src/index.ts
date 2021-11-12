ca41e7fd53c23e878a28610b150d8eda0167dd29
samroodAli <devsamrood@gmail.com>
2021-11-12 18:24:57 +0530

    remove grunt test console statement

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,3 @@ export * from "./middlewares/current-user"
 export * from "./middlewares/error-handler"
 export * from "./middlewares/require-auth"
 export * from "./middlewares/validate-request"
-
-console.log("test grunt")
