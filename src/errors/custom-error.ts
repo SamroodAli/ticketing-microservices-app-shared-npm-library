5e2c22a0a407bfd6b0aab58b1a4379e1c3958bfa
samroodAli <devsamrood@gmail.com>
2021-11-17 18:43:51 +0530

    create events listener, publish base classes and type defintions for events

--- src/errors/custom-error.ts
+++ src/errors/custom-error.ts
@@ -2,7 +2,7 @@ export abstract class CustomError extends Error {
   abstract statusCode: number;
 
   constructor(message: string) {
-    // for error logging purposes
+    // for error logging purposes, to stderr
     super(message);
     // since we are extending a native class, 'Error'
     Object.setPrototypeOf(this, CustomError.prototype);
