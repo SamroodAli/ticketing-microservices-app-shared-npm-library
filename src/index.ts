3393e88e5d4245671ce56ca2ccfff8a3832d6a5b
samroodAli <devsamrood@gmail.com>
2021-12-17 19:35:54 +0530

    event: create definition for order updated event and export it

--- src/index.ts
+++ src/index.ts
@@ -17,5 +17,6 @@ export * from "./events/definitions/ticket-updated-event";
 export * from "./events/helper-types/order-status";
 export * from "./events/definitions/order-created.-event";
 export * from "./events/definitions/order-cancelled-event";
+export * from "./events/definitions/order-updated-event";
 export * from "./events/definitions/expiration-complete-event";
 export * from "./events/definitions/payment-created-event";
